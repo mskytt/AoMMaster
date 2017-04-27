8e558986efb45c995c1a3d7b2cebe388373db0b6
Måns Skytt <mans.skytt@gmail.com>
2017-04-27 15:04:53 +0200

    Modified ignore file to ignore xlsx temps and .pyc

--- forwardCurves.py
+++ forwardCurves.py
@@ -19,14 +19,3 @@ OISdataInd = OISdata.index # type: pandas.tseries.index.DatetimeIndex
 OISdataCol = OISdata.columns # type: pandas.indexes.base.Index
 OISdataMat = OISdataDF.values # type: numpy.ndarray
 
-LookupColumn = 'EUREON2W='
-LookupTime = '2012-04-09'
-LookupTime3 = '2012-04-06'
-LookupTime2 = OISdata.index[0:5]
-
-#OISdataDFNoNa = OISdataDF.fillna(method='backfill', axis=0, limit=4) # Fill nan  
-
-DataExtract = OISdataFilled.extractData(LookupColumn, LookupTime, True, False)
-np.set_printoptions(threshold='nan')
-print OISdata.dflinterp
-print DataExtract
