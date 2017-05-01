77e81a06a4c62ed92f044637bc03897be30b7fa8
Måns Skytt <mans.skytt@gmail.com>
2017-05-01 13:13:40 +0200

    forward curves computed correct. Added lists with test data to verify functionality

--- forwardCurves.py
+++ forwardCurves.py
@@ -2,6 +2,7 @@
 """
     - OIS data processing 
     - Building forward interest rate curves
+    
 
     v0.1 - Mans Skytt
 """
@@ -42,29 +43,41 @@ def OIStoZeroCoupon(maturityDates, OISrates):
     return ZCrates[0,:] # Ugly but needed due to allocation of matrix! gonna try to switch allocation
 
 def ZeroCoupontoForward(maturityDates, ZCrates):
-    forwardRate = 0 # manipulation of ZC to get forward rates
-    deltaTs = [j-i for i, j in zip(maturityDates[:-1], maturityDates[1:])]
-
+    # manipulation of ZC to get forward rates
+    forwardRates = np.zeros(ZCrates.shape)
+    forwardRates[0] = ZCrates[0] # First forward rate = first ZC rate 
+    i = 1
+    while i < ZCrates.size:
+        forwardRates[i] = (ZCrates[i]*maturityDates[i] - ZCrates[i-1]*maturityDates[i-1])/(maturityDates[i]-maturityDates[i-1])
+        i += 1
+    return forwardRates
 
 
 matDates = [1/52, 2/52,3/52,1/12,2/12,3/12,4/12,5/12,6/12,7/12,8/12,9/12,10/12,11/12,1,15/12,18/12,21/12,2,3,4,5,6,7,8,9,10] #,12,15,20,30,40,50]
 
-y = OIStoZeroCoupon(matDates,OISdataMat[0,0:len(matDates)])
-cs = CubicSpline(matDates,y)
-xs = np.arange(min(matDates), max(matDates), 1/365)
-print ZeroCoupontoForward(matDates,y)
+matDatesTest = [0.083333333, 0.166666667, 0.25, 0.5, 0.75, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+OISTest = [-0.00361, -0.00403, -0.00414, -0.00404, -0.00401, -0.00394, -0.00264, -0.00097, 0.00101, 0.00315, 0.0054, 0.008125, 0.009875, 0.01065, 0.0129]
 
+ZCrates = OIStoZeroCoupon(matDatesTest,OISTest)
+forwardRates = ZeroCoupontoForward(matDatesTest,ZCrates)
+cs = CubicSpline(matDatesTest,forwardRates)
+xs = np.arange(min(matDatesTest), max(matDatesTest), 1/365)
+print type(forwardRates)
+print forwardRates
+"""
 plt.axis([0,max(matDates),-0.4,1.7]) # lock axis
 plt.ion()
 row = 3000
 while row < 3500: 
-    y = OIStoZeroCoupon(matDates,OISdataMat[row,0:len(matDates)])
-    cs = CubicSpline(matDates,y)
+    ZCrates = OIStoZeroCoupon(matDates,OISdataMat[row,0:len(matDates)])
+    forwardRates = ZeroCoupontoForward(matDates,ZCrates)
+    cs = CubicSpline(matDates,forwardRates)
     xs = np.arange(min(matDates), max(matDates), 1/365)
         
-    plt.plot(matDates, y, 'o', label='data')
+    plt.plot(matDates, forwardRates, 'o', label='data')
     plt.plot(xs, cs(xs), label='spline')
     plt.title(OISdataDF.index[row])
     plt.pause(0.001)
     plt.clf()
     row += 1
+"""
