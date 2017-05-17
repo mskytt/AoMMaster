7c3c8adb59400b022a72d0d04a8c47081311dcd8
Anna Dunblad <anndu960@student.liu.se>
2017-05-17 12:58:28 +0200

    All functionality working in creating future and forward positions, now I need to bind everythong togheter in a better way. Also plots

--- createHistPositions.py
+++ createHistPositions.py
@@ -6,7 +6,62 @@ import os.path
 from pdb import set_trace
 import datetime
 
-	
+
+
+
+
+
+def getInterestRateDates(EONIA = True, FFE = False):
+	if EONIA:
+		pathToData = 'Data/OIS_Data.xlsx'
+		sheet = 'EONIA_MID'
+		maturitiesInterest = loadFromHDF5('EONIAask.hdf5','times') #maturities, column wise of interest data
+		datesInterest = pd.read_excel(pathToData,sheet ,parse_cols = [0]) #dates, row wise of interest rate data
+		datesInterest['dates'] = pd.to_datetime(datesInterest['dates'])  # pandas.core.frame.DataFrame of 'numpy.datetime64'> elements
+		return datesInterest
+	else: #TODO
+		return []
+
+def getInterestRates():
+	return loadFromHDF5('EONIAask.hdf5','MATLABForwardMat') #interest rates data, type
+
+
+
+ #----------USAGE --------
+
+def useData(dfFuturesData,ForwarRatesdMat,datesInterest):
+	interestRates = getCorrespondingInterestRates(dfFuturesData,ForwarRatesdMat,datesInterest)
+
+
+
+
+ 
+def getCorrespondingInterestRates(dfFuturesData,ForwarRatesdMat,datesInterest):
+	column = dfFuturesData.columns[0] #for column in dfFuturesData.columns:
+	datesFutures = dfFuturesData[column].dropna().index #<class 'pandas.tseries.index.DatetimeIndex'
+	datesFutures = np.array(datesFutures, dtype = 'datetime64[ns]') #<type 'numpy.ndarray'> of <numpy.datetime64'> elements
+	interestRates = []
+	ForwarRatesdMat_index = []
+	_datesFutures = []
+	_unusedDatesFutures = []
+	timeToMats = []
+	timeToMat = len(datesFutures)
+	print "finding corresponding interest rates for future " + column
+	i = 0
+	for date in datesFutures:
+		i += 1
+		if datesInterest[datesInterest['dates'].values == date].index.tolist(): #spara 'ven vilka datum som finns'
+			#print datesInterest[datesInterest['dates'].values == date].index.tolist()
+			ForwarRatesdMat_index.append(datesInterest[datesInterest['dates'].values == date].index.tolist()[0])
+
+			_datesFutures.append(date) #only save those who have a corresponding interest rate date
+			timeToMats.append(timeToMat - i) 
+		else: 
+			 _unusedDatesFutures.append(datesInterest[datesInterest['dates'].values == date].index.tolist())
+
+	return ForwarRatesdMat[ForwarRatesdMat_index,timeToMat]
+
+
 
 def getForwardPosition(strikePrice, maturityPrice):
 	return  maturityPrice - strikePrice
@@ -19,13 +74,10 @@ def getFuturesPosition(futurePrices,interestRates):
 		futuresPosition += (futuresPrices.values[i] - strike)*interestRates.values[i]*timeToMat
 	return futuresPosition
 
-def getMaturity_inDays(col_name,dates):
-	return  (dates.loc[col_name, u'FINAL SETTLE DATE'] -  dates.loc[col_name, u'FUT.1ST PRICE DATE']).days
+def getMaturity_inDays(name,dates):
+	return  (dates.loc[name, u'FINAL SETTLE DATE'] -  dates.loc[name, u'FUT.1ST PRICE DATE']).days
 
 	
-
-
-
 # --------------- start program ---------------
 
 # CHOOSE COMMODITY
@@ -33,27 +85,26 @@ gold = True
 aluminimum = False
 oil = False
 power = False
-EONIA = True
 
 
 # -----------------------------------------------
 if gold:
-	#data
+	#interest rate dates and data
+	datesInterest = getInterestRateDates(EONIA = True, FFE = False)
+	ForwarRatesdMat = getInterestRates()
+
+	#futures dates and data
 	pathToData =  'Data/GoldFutures.xlsx'
-	sheet_specs = 'COMEXGoldFutSpecs'
-	index_specs = 0
-	dfMaturitiesFutures = pd.read_excel(pathToData,sheet_specs, index_col =index_specs ,parse_cols = [0,2,3])
-
-
 	sheets = ['ReutersCOMEXGoldTS1', 'ReutersCOMEXGoldTS2', 'ReutersCOMEXGoldTS3']
 	indexColumn = 0
 	dfFuturesData = pd.DataFrame()
 	for sheet in sheets[0:1]:
 		xlsFuturesData = xlExtract(pathToData,sheet,indexColumn)
-		dfFuturesData = dfFuturesData.join(xlExtract.extractData(xlsFuturesData, xlsFuturesData.columns,'2017-04-21',  entireTS = True, useLinterpDF = True).dropna(how = 'all'),how = 'outer')
+		dfFuturesData = xlExtract.extractData(xlsFuturesData, xlsFuturesData.columns,'2017-04-21',  entireTS = True, useLinterpDF = False).dropna(how = 'all')
 		print "sheet " + str(sheet) + " extracted"	
+		useData(dfFuturesData,ForwarRatesdMat,datesInterest)
+		
 
-	 	
 if aluminimum:
 	pass
 
@@ -73,64 +124,9 @@ if power:
 	print "reading sheet  " + str(sheet)
 
 	dfFuturesData = xlExtract.extractData(xlExtract(pathToData,sheet[0],indexColumn), xlsFuturesData.columns,'24-03-2017',  entireTS = True, useLinterpDF = True)
-	dfFuturesData_new = dfFuturesData.join(xlExtract.extractData(xlExtract(pathToData,sheet[1],indexColumn), xlsFuturesData.columns,'24-03-2017',  entireTS = True, useLinterpDF = True))
+	dfFuturesData = dfFuturesData.join(xlExtract.extractData(xlExtract(pathToData,sheet[1],indexColumn), xlsFuturesData.columns,'24-03-2017',  entireTS = True, useLinterpDF = True))
 	storeToHDF5('PowerFutures.hdf5','PowerFutures', dfFuturesData)		
 	EONIA = True
-
-if EONIA:
-	pathToData = 'Data/OIS_Data.xlsx'
-	sheet = 'EONIA_MID'
-	ForwarRatesdMat = loadFromHDF5('EONIAask.hdf5','MATLABForwardMat') #interest rates data, type
-	maturitiesInterest = loadFromHDF5('EONIAask.hdf5','times') #maturities, column wise of interest data
-	datesInterest = pd.read_excel(pathToData,sheet ,parse_cols = [0]) #dates, row wise of interest rate data
-	datesInterest['dates'] = pd.to_datetime(datesInterest['dates']) 
-
-
-
-
-# ----- FUTURES -----
-#for column in dfFuturesData.columns:
-column = dfFuturesData.columns[0]
-oneFutures = dfFuturesData[column].dropna() #remove all NANs from calculations
-timeToMat = getMaturity_inDays(column,dfMaturitiesFutures)
-datesFutures = oneFutures.index
-
-# # print "------- dates interest --------"
-# # print datesInterest
-
-# # print "------- datesInterest.index(date) --------"
-# # date = datesFutures[0]
-
-# date = datesInterest.iloc[3] 
-# print type(date.values)
-# print date.values
-# # print "-------   date = datesInterest.iloc[3] --------"
-
-# #print datesInterest.loc['2016-08-01']
-# # print date.values
-# #print datesInterest[datesInterest.values == date.values].index.tolist()
-
-
-# 	#find matching interest rate
-
-# #goal: return the interest rate to invest in
-
-interestRates = []
-ForwarRatesdMat_index = []
-for date in datesFutures[0:1]:
-	print type(date)
-	print date
-
-	#print date
-
-	print ForwarRatesdMat_index.append(datesInterest[datesInterest.values == date].index.tolist())
-	#interestRates = ForwarRatesdMat(datesFutures.index)
-
-
-
-
-	
-
 
 
 
